88495cda02378a471d78da75547e52f98080a78f
MartinMinuto <martin.minuto1@gmail.com>
2023-04-21 20:00:24 -0300

    1.5

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './Components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './Components/media-player/media-player.component';
+import { HeaderUserComponent } from './Components/header-user/header-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
