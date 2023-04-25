99d9bbc5eafee500ae70c709051bb32ab25ca3a9
MartinMinuto <martin.minuto1@gmail.com>
2023-04-24 22:13:33 -0300

    1.11

--- src/app/modules/tracks/pages/tracks-pag/tracks-pag.component.ts
+++ src/app/modules/tracks/pages/tracks-pag/tracks-pag.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from '../../../../data/tracks.json'
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-tracks-pag',
   templateUrl: './tracks-pag.component.html',
   styleUrls: ['./tracks-pag.component.css']
 })
-export class TracksPagComponent {
-   mockTracksList = [
-    {
-      name: 'Hola',
-    },
-    {
-      name: 'Hola',
-    }
+export class TracksPagComponent implements OnInit {
+   mockTracksList:Array<TrackModel> = [
+
    ]
+
+   ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.mockTracksList = data;
+   }
 
 }
