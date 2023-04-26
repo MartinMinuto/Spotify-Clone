c3f82dac777ff4f4cfd90943978847939ca8c8dc
MartinMinuto <martin.minuto1@gmail.com>
2023-04-26 00:21:23 -0300

    1.15

--- src/app/modules/tracks/pages/tracks-pag/tracks-pag.component.ts
+++ src/app/modules/tracks/pages/tracks-pag/tracks-pag.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import * as dataRaw from '../../../../data/tracks.json'
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { TrackService } from '@modules/tracks/services/track.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tracks-pag',
   templateUrl: './tracks-pag.component.html',
   styleUrls: ['./tracks-pag.component.css']
 })
-export class TracksPagComponent implements OnInit {
-   mockTracksList:Array<TrackModel> = [
+export class TracksPagComponent implements OnInit, OnDestroy {
+   tracksTrending:Array<TrackModel> = []
+   tracksRandom:Array<TrackModel> = []
 
-   ]
+   listObservers$: Array<Subscription> = []
+
+   constructor(private trackService: TrackService) { }
 
    ngOnInit(): void {
-    const { data }: any = (dataRaw as any).default
-    this.mockTracksList = data;
+     const observer1$ = this.trackService.dataTracksTrending$
+     .subscribe(response => {
+      this.tracksTrending = response
+     })
+
+     const observer2$ = this.trackService.dataTracksRandom$
+     .subscribe(response => {
+      this.tracksRandom = [...this.tracksRandom, ...response]
+     })
+
+     this.listObservers$ = [observer1$, observer2$]
+   }
+
+   ngOnDestroy(): void {
+     this.listObservers$.forEach(u => u.unsubscribe())
    }
 
 }
--- src/app/shared/Components/media-player/media-player.component.ts
+++ src/app/shared/Components/media-player/media-player.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
   mockCover: TrackModel = {
     cover: 'https://jenesaispop.com/wp-content/uploads/2009/09/guetta_onelove.jpg',
     album: 'title',
     name: 'title',
     url: 'https://LocalHosts',
     _id: 1
-
   }
+
+  listObservers$: Array<Subscription> = []
 
   constructor(private multimediaService: MultimediaService) {}
 
   ngOnInit(): void {
-    
+    const observer1$: Subscription = this.multimediaService.callback.subscribe(
+      (response: TrackModel) => {
+        
+      }
+    )
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe())
   }
 }
